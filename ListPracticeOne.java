91fb5edf340a9d8ee9ec7427870a87375ae60607
adyagupta <45395088+adyagupta@users.noreply.github.com>
2018-11-27 10:39:58 -0500

    Set random values
    Set the lists to have 10 random values between 1 and 25. Created a combine lists method.

--- ListPracticeOne.java
+++ ListPracticeOne.java
@@ -5,14 +5,17 @@ public class ListPracticeOne{
 			ArrayList<Integer>list2 = new ArrayList<>();
 			ArrayList<Integer>list3 = new ArrayList<>();
 
-			for(int i  = 1; i<11; i++)
-				list1.add(i);
-			for(int i  = 1; i<11; i++)
-				list2.add(i);
+			for(int i = 0; i<10; i++)
+				list1.add((int)(Math.random()*25));
 
-			System.out.println(list1);
-			System.out.println(list2);
+			for(int i = 0; i<10; i++)
+				list2.add((int)(Math.random()*25));
+
+			System.out.println(list3);
 	}
-	//public static ArrayList<Integer>combineList(ArrayList<Integer>list1, ArrayList<Integer>list2){
-	//}
+	public static ArrayList<Integer>combineList(ArrayList<Integer>list1, ArrayList<Integer>list2, ArrayList<Integer>list3){
+
+			return list3;
+
+	}
 }
